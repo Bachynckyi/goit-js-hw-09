4477e1a539ddb351f6728abca5041c1fa2ac8bbe
Andrew <bachynckyi.andrii@gmail.com>
2022-07-25 19:21:15 +0300

    task_1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
 
+const buttonStart = document.querySelector('.buttonStart');
+const buttonStop = document.querySelector('.buttonStop');
+const bodyColor = document.querySelector('body');
+
+buttonStart.addEventListener("click", onStart);
+buttonStop.addEventListener("click", onStop);
+
+buttonStop.setAttribute('disabled', true);
+
+function onStart() {
+  timerId = setInterval(() => {
+  const randomColor = getRandomHexColor();
+  bodyColor.style.backgroundColor = randomColor;
+  }, 1000);
+  buttonStart.setAttribute('disabled', true);
+  buttonStop.removeAttribute('disabled');
+};
+
+function onStop() {
+  clearInterval(timerId);
+  buttonStop.setAttribute('disabled', true);
+  buttonStart.removeAttribute('disabled');
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
